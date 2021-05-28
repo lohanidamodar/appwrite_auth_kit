40728d570184a2f61335285120245fcd5289758f
Damodar Lohani <lohanidamodar@users.noreply.github.com>
2021-05-28 11:11:33 +0545

    updates

--- lib/src/accounts_provider.dart
+++ lib/src/accounts_provider.dart
@@ -203,10 +203,12 @@ class AuthNotifier extends ChangeNotifier {
     }
   }
 
-  Future<bool> createOAuth2Session({ required String provider,
-      String success = 'https://appwrite.io/auth/oauth2/success',
-      String failure = 'https://appwrite.io/auth/oauth2/failure',
-      List scopes = const [],}) async {
+  Future<bool> createOAuth2Session({
+    required String provider,
+    String success = 'https://appwrite.io/auth/oauth2/success',
+    String failure = 'https://appwrite.io/auth/oauth2/failure',
+    List scopes = const [],
+  }) async {
     try {
       await _account.createOAuth2Session(provider: provider, success: success, failure: failure);
       _getUser();
